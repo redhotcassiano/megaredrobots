36c53c9d0e33b803b7cc1eb0ee29bb64a630171e
Cassiano de Souza <redhotcassiano@gmail.com>
2019-03-20 16:20:17 -0300

    Criação do componete de pesquisa.

--- src/main.js
+++ src/main.js
@@ -2,6 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import BootstrapVue from 'bootstrap-vue'
+import BContainer from 'bootstrap-vue/es/components/layout/container'
+import BForm from 'bootstrap-vue/es/components/form/form'
+import BFormInput from 'bootstrap-vue/es/components/form-input/form-input'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+
+Vue.component('b-container', BContainer)
+Vue.component('b-form', BForm)
+Vue.component('b-form-input', BFormInput)
 
 Vue.config.productionTip = false
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/robots.jpg">
+    <HelloWorld msg="Mega Red Robots"/>
   </div>
 </template>
 
